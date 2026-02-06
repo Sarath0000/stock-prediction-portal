2907d8eefec04436e2dad31a34d85684fb9dd725
sarath <sarathpa2002@gmail.com>
2026-02-06 18:55:08 +0530

    homepage

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+        <div className='container'>
+            <div className='p-5 text-center bg-light-dark rounded'>
+                <h1 className='text-light'>Stock Prediction Portal</h1>
+                <p className='text-light lead'>To use Bootstrap 5 in your project, include the following Content Delivery Network (CDN) links for CSS and JavaScript in your HTML file. This method is free, fast, and doesn't require downloading any files. </p>
+                <Button text='Login' class='btn-outline-info'/>
+
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Main
